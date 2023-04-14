eae06fc1c2038f670a7a0ef0065c6d59e2740d14
Ajink Gupta <76146259+Ajinkgupta@users.noreply.github.com>
2023-04-14 19:44:16 +0530

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,1 @@
+self.addEventListener("fetch",function(event){console.log(event.request.url);event.respondWith(caches.match(event.request).then(function(response){return response||fetch(event.request);}));});
