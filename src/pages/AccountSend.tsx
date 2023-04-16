ef79fc37e70f7a9f2e97f141e4bb4e62831de212
Ajink Gupta <76146259+Ajinkgupta@users.noreply.github.com>
2023-04-16 13:15:36 +0530

    Update AccountSend.tsx

--- src/pages/AccountSend.tsx
+++ src/pages/AccountSend.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { sendToken } from '../utils/TransactionUtils';
 import { sepolia } from '../models/Chain';
 import { Account } from '../models/Account';
@@ -6,31 +6,40 @@ import AccountTransactions from './AccountTransactions';
 import { ethers } from 'ethers';
 import { toFixedIfNecessary } from '../utils/AccountUtils';
 import './Account.css';
+import { useLocation } from 'react-router-dom';
 
 interface AccountSendProps {
-  account: Account
+  account: Account;
 }
 
-const AccountSend: React.FC<AccountSendProps> = ({account}) => {
+const AccountSend: React.FC<AccountSendProps> = ({ account }) => {
+  const location = useLocation();
   const [destinationAddress, setDestinationAddress] = useState('');
   const [amount, setAmount] = useState(0);
-  const [balance, setBalance] = useState(account.balance)
+  const [balance, setBalance] = useState(account.balance);
 
-  const [networkResponse, setNetworkResponse] = useState<{ status: null | 'pending' | 'complete' | 'error', message: string | React.ReactElement }>({
+  const [networkResponse, setNetworkResponse] = useState<{
+    status: null | 'pending' | 'complete' | 'error';
+    message: string | React.ReactElement;
+  }>({
     status: null,
     message: '',
   });
 
   useEffect(() => {
     const fetchData = async () => {
-        const provider = new ethers.providers.JsonRpcProvider(sepolia.rpcUrl);
-        let accountBalance = await provider.getBalance(account.address);
-        setBalance((String(toFixedIfNecessary(ethers.utils.formatEther(accountBalance)))));
-    }
+      const provider = new ethers.providers.JsonRpcProvider(sepolia.rpcUrl);
+      let accountBalance = await provider.getBalance(account.address);
+      setBalance(
+        String(toFixedIfNecessary(ethers.utils.formatEther(accountBalance)))
+      );
+    };
     fetchData();
-}, [account.address])
+  }, [account.address]);
 
-  function handleDestinationAddressChange(event: React.ChangeEvent<HTMLInputElement>) {
+  function handleDestinationAddressChange(
+    event: React.ChangeEvent<HTMLInputElement>
+  ) {
     setDestinationAddress(event.target.value);
   }
 
@@ -46,15 +55,29 @@ const AccountSend: React.FC<AccountSendProps> = ({account}) => {
     });
 
     try {
-      const { receipt } = await sendToken(amount, account.address, destinationAddress, account.privateKey);
+      const { receipt } = await sendToken(
+        amount,
+        account.address,
+        destinationAddress,
+        account.privateKey
+      );
 
       if (receipt.status === 1) {
         // Set the network response status to "complete" and the message to the transaction hash
         setNetworkResponse({
           status: 'complete',
-          message: <p>Transfer complete! <a href={`${sepolia.blockExplorerUrl}/tx/${receipt.transactionHash}`} target="_blank" rel="noreferrer">
-            View transaction
-            </a></p>,
+          message: (
+            <p>
+              Transfer complete!{' '}
+              <a
+                href={`${sepolia.blockExplorerUrl}/tx/${receipt.transactionHash}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                View transaction
+              </a>
+            </p>
+          ),
         });
         return receipt;
       } else {
@@ -78,90 +101,79 @@ const AccountSend: React.FC<AccountSendProps> = ({account}) => {
     }
   }
 
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const addressParam = queryParams.get('address');
+    if (addressParam) {
+      setDestinationAddress(addressParam);
+    }
+  }, [location.search]);
+
   return (
-   <>
-   
-   <div className="flex-1 flex justify-start   items-center flex-col   p-5 z-40 ">
-     
-      
-     <div className="bg-[#2D2F36] mt-20   w-max-full     sm:w-[30rem]   md:w-[32rem] rounded-3xl p-4 content-box">
-         
-         <div className="px-2 flex items-center text-white justify-between font-semibold text-xl">
-           <span>SEND</span>
- 
-         </div>
-    <div className="bg-[#20242A] my-3 rounded-2xl p-6 text-3xl  border border-[#20242A] hover:border-[#41444F]  flex justify-between">
-        
-    <input
-           className="bg-transparent mr-2 rounded h-10 placeholder:text-[#B2B9D2] outline-none mb-6 w-full text-2xl" 
-            type="number"
-            value={amount}
-            onChange={handleAmountChange}
+    <>
+      <div className="flex-1 flex justify-start   items-center flex-col   p-5 z-40 ">
+        <div className="bg-[#2D2F36] mt-20   w-max-full     sm:w-[30rem]   md:w-[32rem] rounded-3xl p-4 content-box">
+          <div className="px-2 flex items-center text-white justify-between font-semibold text-xl">
+            <span>SEND</span>
+          </div>
+          <div className="bg-[#20242A] my-3 rounded-2xl p-6 text-3xl  border border-[#20242A] hover:border-[#41444F]  flex justify-between">
+            <input
+              className="bg-transparent mr-2 rounded h-10 placeholder:text-[#B2B9D2] outline-none mb-6 w-full text-2xl"
+              type="number"
+              value={amount}
+              onChange={handleAmountChange}
             />
-    
-         
-           <div className="flex w-1/4">
-            
-             <div className="w-full h-12 flex justify-between items-center bg-[#2D2F36] hover:bg-[#41444F] rounded-2xl text-xl font-medium cursor-pointer p-2 mt-[-0.2rem]">
-            
-               <div className="flex text-white items-center">
+
+            <div className="flex w-1/4">
+              <div className="w-full h-12 flex justify-between items-center bg-[#2D2F36] hover:bg-[#41444F] rounded-2xl text-xl font-medium cursor-pointer p-2 mt-[-0.2rem]">
+                <div className="flex text-white items-center">
                   <span className="mx-2">ETH</span>
-               </div>
-              
-             </div>
-           </div>
-         </div>
-        
-         <div className="bg-[#20242A] my-3 rounded-2xl p-6 text-3xl  border border-[#20242A] hover:border-[#41444F]  flex justify-between">
-          
- 
+                </div>
+              </div>
+            </div>
+          </div>
 
-         <input
-           className=" rounded h-10 bg-transparent placeholder:text-[#B2B9D2] outline-none mb-6 w-full text-2xl"
-            placeholder="0x...."  
-            type="text"
-            value={destinationAddress}
-            onChange={handleDestinationAddressChange}
+          <div className="bg-[#20242A] my-3 rounded-2xl p-6 text-3xl  border border-[#20242A] hover:border-[#41444F]  flex justify-between">
+            <input
+              className=" rounded h-10 bg-transparent placeholder:text-[#B2B9D2] outline-none mb-6 w-full text-2xl"
+              placeholder="0x...."
+              type="text"
+              value={destinationAddress}
+              onChange={handleDestinationAddressChange}
             />
-  
-         
-         </div>
-         
-        
+          </div>
 
-     
-         <button
-            className="bg-purple-600 my-2  w-full rounded-3xl py-2 px-8 text-xl font-semibold flex items-center justify-center cursor-pointer border border-black hover:border-[#234169]"  
-           type="button"
+          <button
+            className="bg-purple-600 my-2  w-full rounded-3xl py-2 px-8 text-xl font-semibold flex items-center justify-center cursor-pointer border border-black hover:border-[#234169]"
+            type="button"
             onClick={transfer}
             disabled={!amount || networkResponse.status === 'pending'}
-        >
-            Send {amount} ETH
-        </button>
-
-        
- 
-
-        {networkResponse.status &&
-            <>
-             <div
-            className="bg-orange-600 my-2  w-full rounded-3xl py-2 px-8 text-xl font-semibold flex items-center justify-center cursor-pointer border border-black hover:border-[#234169]"  
           >
-             {networkResponse.status === 'pending' && <p>Transfer is pending...</p>}
-            {networkResponse.status === 'complete' && <p>{networkResponse.message}</p>}
-            {networkResponse.status === 'error' && <p>Error occurred while transferring tokens: {networkResponse.message}</p>}
-           
+            Send {amount} ETH
+          </button>
+
+          {networkResponse.status && (
+            <>
+              <div className="bg-orange-600 my-2  w-full rounded-3xl py-2 px-8 text-xl font-semibold flex items-center justify-center cursor-pointer border border-black hover:border-[#234169]">
+                {networkResponse.status === 'pending' && (
+                  <p>Transfer is pending...</p>
+                )}
+                {networkResponse.status === 'complete' && (
+                  <p>{networkResponse.message}</p>
+                )}
+                {networkResponse.status === 'error' && (
+                  <p>
+                    Error occurred while transferring tokens:{' '}
+                    {networkResponse.message}
+                  </p>
+                )}
+              </div>
+            </>
+          )}
         </div>
-
-             </>
-        }
- 
-       </div>
-   </div>
-   
-   </>
-
-  )
-}
+      </div>
+    </>
+  );
+};
 
 export default AccountSend;
